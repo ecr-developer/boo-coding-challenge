4a7befb00ed4c9f0f213efaa221c46a3d1bd7abb
Emerson Rodrigues <ecr.developer23@gmail.com>
2024-01-14 01:33:53 +0100

    feat: Add db in-memory for tests

---
+++ server/src/core/shared/domain/__tests__/aggregate-root.spec.ts
@@ -0,0 +1,56 @@
+import { AggregateRoot } from '../aggregate-root';
+import { Uuid } from '../value-objects/uuid.vo';
+
+class StubEvent {
+  occurred_on: Date;
+  event_version: number = 1;
+
+  constructor(
+    public aggregate_id: Uuid,
+    public name: string,
+  ) {
+    this.occurred_on = new Date();
+    this.name;
+  }
+}
+
+class StubAggregateRoot extends AggregateRoot {
+  aggregate_id: Uuid;
+  name: string;
+  field1: string;
+
+  constructor(name: string, id: Uuid) {
+    super();
+    this.aggregate_id = id;
+    this.name = name;
+  }
+
+  get entity_id() {
+    return this.aggregate_id;
+  }
+
+  operation() {
+    this.name = this.name.toUpperCase();
+  }
+
+  onStubEvent(event: StubEvent) {
+    this.field1 = event.name;
+  }
+
+  toJSON() {
+    return {
+      aggregate_id: this.aggregate_id,
+      name: this.name,
+      field1: this.field1,
+    };
+  }
+}
+
+describe('AggregateRoot Unit Tests', () => {
+  test('dispatch events', () => {
+    const id = new Uuid();
+    const aggregate = new StubAggregateRoot('test name', id);
+    aggregate.operation();
+    expect(aggregate.field1).toBe('TEST NAME');
+  });
+});
--- server/src/core/shared/domain/repository/repository-interface.ts
+++ server/src/core/shared/domain/repository/repository-interface.ts
@@ -6,8 +6,6 @@ import { SearchResult } from './search-result';
 export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   insert(entity: E): Promise<void>;
   bulkInsert(entities: E[]): Promise<void>;
-  update(entity: E): Promise<void>;
-  delete(entity_id: EntityId): Promise<void>;
 
   findById(entity_id: EntityId): Promise<E | null>;
   findAll(): Promise<E[]>;
