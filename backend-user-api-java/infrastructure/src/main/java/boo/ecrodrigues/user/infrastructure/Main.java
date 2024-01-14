c9a8895c4e4cd5f041a6291f6de289e72282d235
Emerson Rodrigues <ecr.developer23@gmail.com>
2024-01-15 00:17:59 +0100

    fix(backend): Change gradle version

--- backend-user-api-java/infrastructure/src/main/java/boo/ecrodrigues/user/infrastructure/Main.java
+++ backend-user-api-java/infrastructure/src/main/java/boo/ecrodrigues/user/infrastructure/Main.java
@@ -1,8 +1,21 @@
 package boo.ecrodrigues.user.infrastructure;
 
+import boo.ecrodrigues.user.infrastructure.configuration.WebServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+@SpringBootApplication
 public class Main {
 
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    LOG.info("[step:to-be-init] [id:1] Initializing Spring");
+    System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+    SpringApplication.run(WebServerConfig.class, args);
+    LOG.info("[step:inittialized] [id:2] Spring inittialized...");
   }
 }
---
+++ backend-user-api-java/infrastructure/src/main/java/boo/ecrodrigues/user/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,10 @@
+package boo.ecrodrigues.user.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("boo.ecrodrigues.user")
+public class WebServerConfig {
+
+}
