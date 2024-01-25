cdb24cb5af92d37ad2bc9c70dd469912d2299c4c
Emerson Rodrigues <ecr.developer23@gmail.com>
2024-01-25 14:50:01 +0100

    fix: sandbox environments for app and service

--- backend-user-api-java/infrastructure/src/main/java/boo/ecrodrigues/user/infrastructure/Main.java
+++ backend-user-api-java/infrastructure/src/main/java/boo/ecrodrigues/user/infrastructure/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
   public static void main(String[] args) {
     LOG.info("[step:to-be-init] [id:1] Initializing Spring");
-    System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+    System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "production");
     SpringApplication.run(WebServerConfig.class, args);
     LOG.info("[step:inittialized] [id:2] Spring inittialized...");
   }
